b275e99d0c60a214141898d6509ee552bcf42ee1
Alexey Ledovskiy <alexey@concreteinteractive.com>
2013-06-25 17:11:15 +0200

    course list screen

--- l2p_dropbox_downloader/gen/de/rwth/elearning/l2p_dropbox_downloader/R.java
+++ l2p_dropbox_downloader/gen/de/rwth/elearning/l2p_dropbox_downloader/R.java
@@ -24,14 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int CourseTextView=0x7f080000;
+        public static final int action_settings=0x7f080005;
         public static final int button=0x7f080002;
-        public static final int listView=0x7f080000;
-        public static final int textView1=0x7f080001;
+        public static final int l2pRoomButton=0x7f080004;
+        public static final int l2pTextView=0x7f080003;
+        public static final int listView=0x7f080001;
     }
     public static final class layout {
         public static final int activity_courselist=0x7f030000;
         public static final int activity_login=0x7f030001;
+        public static final int room_list_item=0x7f030002;
     }
     public static final class menu {
         public static final int login=0x7f070000;
@@ -39,7 +42,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int button=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int textview=0x7f050003;
     }
     public static final class style {
         /** 
--- l2p_dropbox_downloader/src/de/rwth/elearning/l2p_dropbox_downloader/CourseListActivity.java
+++ l2p_dropbox_downloader/src/de/rwth/elearning/l2p_dropbox_downloader/CourseListActivity.java
@@ -1,16 +1,18 @@
 package de.rwth.elearning.l2p_dropbox_downloader;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by Alex on 6/23/13.
@@ -18,66 +20,102 @@ import android.widget.ListView;
 public class CourseListActivity extends Activity {
 	private ListView listView;
 
-    public void onCreate(Bundle savedInstanceState) {
+	private static final String l2pLink = "https://www2.elearning.rwth-aachen.de";
+	ListView roomList;
+	RoomArrayAdapter adapter;
+	List<LearnRoom> l2pRoomslist;
+	
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courselist);
         listView = (ListView) findViewById(R.id.listView);
         
         String[] values = new String[] { "Course1", "Course2" };
 
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-          list.add(values[i]);
+        final ArrayList<LearnRoom> list = new ArrayList<LearnRoom>();
+        for (int i = 0; i < values.length; ++i) 
+        {
+          LearnRoom a = new LearnRoom();
+          a.setName(values[i]);
+        	list.add(a);
         }
-        final StableArrayAdapter adapter = new StableArrayAdapter(this,
-            android.R.layout.simple_list_item_1, list);
+        
+        final RoomArrayAdapter adapter = new RoomArrayAdapter(this, R.layout.room_list_item, list);
         listView.setAdapter(adapter);
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-          @Override
-          public void onItemClick(AdapterView<?> parent, final View view,
+        @Override
+        public void onItemClick(AdapterView<?> parent, final View view,
               int position, long id) {
             final String item = (String) parent.getItemAtPosition(position);
-            view.animate().setDuration(2000).alpha(0)
-                .withEndAction(new Runnable() {
-                  @Override
-                  public void run() {
-                    list.remove(item);
-                    adapter.notifyDataSetChanged();
-                    view.setAlpha(1);
-                  }
-                });
+            startL2pSections(l2pLink+item);
           }
-
-
         });
     }
-    
-    private class StableArrayAdapter extends ArrayAdapter<String> {
 
-        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+	private void startL2pSections(String Url){
+		//Intent l2psections = new Intent(this,Sections.class);
 
-        public StableArrayAdapter(Context context, int textViewResourceId,
-            List<String> objects) {
-          super(context, textViewResourceId, objects);
-          for (int i = 0; i < objects.size(); ++i) {
-            mIdMap.put(objects.get(i), i);
-          }
-        }
+		//l2psections.putExtra("url", Url);
+		//startActivity(l2psections);
+	}
+	
+	
+    private class RoomArrayAdapter extends ArrayAdapter<LearnRoom> 
+    {
+		//private static final String tag = "L2pRoomArrayAdapter";
+		private Context context;
+		private List<LearnRoom> objects;
+		
+		public RoomArrayAdapter(Context context, int textViewResourceId, List<LearnRoom> objects) {
+			super(context, textViewResourceId, objects);
+			this.context = context;
+			this.objects = objects;
+		}
 
-        @Override
-        public long getItemId(int position) {
-          String item = getItem(position);
-          return mIdMap.get(item);
-        }
+		public int getCount() {
+			return this.objects.size();
+		}
 
-        @Override
-        public boolean hasStableIds() {
-          return true;
-        }
+		public LearnRoom getItem(int index) {
+			return this.objects.get(index);
+		}
 
-      }
+		public View getView(int position, View convertView, ViewGroup parent) 
+		{
+			View row = convertView;
+			TextView tw1=(TextView)findViewById(R.id.l2pTextView);
+			tw1.setText(((LearnRoom)objects.get(position)).getName());
+			if (row == null) {
+				//Log.d(tag, "Starting XML Row Inflation ... ");
+				LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);;
+				row = inflater.inflate(R.layout.room_list_item, parent, false);
+				//Log.d(tag, "Successfully completed XML Row Inflation!");
+			}
 
-    } 
+//			// Get item
+//			LearnRoom learnroom = getItem(position);
+//			if(learnroom != null){
+//				// Get reference to Buttons
+//				Button l2pButton = (Button)row.findViewById(R.id.l2pRoomButton);
+//				TextView l2pnameText = (TextView)row.findViewById(R.id.l2pTextView);
+//				
+//				//Set Text and Tags
+//				//Tags are important because they allow us to identify the Clicked Object later.
+//				if(l2pnameText != null){
+//					l2pnameText.setText(learnroom.getName());
+//				}
+//				if(l2pButton != null){
+//					l2pButton.setTag(learnroom.getlinkl2p());
+//					//
+//				}
+//
+//			}
+			return row;
+		}
+    }
+}
+
+ 
     
