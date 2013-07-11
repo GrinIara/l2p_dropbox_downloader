5cca9d7e8afcbe390194b19c69fc7eadbab5ef12
Alexey Ledovskiy <alexey@concreteinteractive.com>
2013-07-11 13:13:11 +0200

    login

--- l2p_dropbox_downloader/src/com/dropbox/android/sample/LoginActivity.java
+++ l2p_dropbox_downloader/src/com/dropbox/android/sample/LoginActivity.java
@@ -9,14 +9,19 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
-
+	
+	EditText textEditUser;
+	EditText textEditPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Button login = (Button) findViewById(R.id.btn_login);
+        textEditUser = (EditText) findViewById(R.id.et_un);
+        textEditPass = (EditText) findViewById(R.id.et_pw);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,10 +35,11 @@ public class LoginActivity extends Activity {
 		SharedPreferences.Editor editor = app_preferences.edit();
        	
 		//PUT YOUR CREDENTIALS HERE INSTEAD OF login and pass
-		
+		String login = textEditUser.getText().toString();
+		String pass = textEditPass.getText().toString();
 		//Important: When you login you need to save login and pass of a user here
-		editor.putString("LoginL2P", "login");
-       	editor.putString("PassL2P", "pass");
+		editor.putString("LoginL2P", login);
+       	editor.putString("PassL2P", pass );
 
        	editor.commit(); // Very important
         Intent i = new Intent(getBaseContext(),CourseListActivity.class);
